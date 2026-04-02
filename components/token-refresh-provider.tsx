e72f0117ae5b94889fb8654a1e679ed3d60af27a
Arnav Verma <arnavverma522@gmail.com>
2026-04-02 01:04:33 -0500

    feat: proactive JWT token refresh before expiry

---
+++ components/token-refresh-provider.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import { useTokenRefresh } from "@/hooks/use-token-refresh"
+
+export function TokenRefreshProvider({ children }: { children: React.ReactNode }) {
+  useTokenRefresh()
+  return <>{children}</>
+}
